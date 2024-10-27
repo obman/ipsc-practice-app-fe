e54ab145dfc742bba8d31ad23ba9ad7898ef95f4
denis.music <deniss9oo@gmail.com>
2024-10-27 17:55:24 +0100

    Created dropdown navigation for logged in users

--- src/components/layouts/front-header.vue
+++ src/components/layouts/front-header.vue
@@ -55,15 +55,22 @@ export default defineComponent({
       v-if="username"
       class="relative">
         <span
-          class="nav-item text-white hover:text-primary"
+          class="nav-item flex justify-start items-center text-white hover:text-primary cursor-pointer"
           @click="profileMenuToggle">
-          Hi, {{ username }}
+          <span class="mr-1">Hi, {{ username }}</span>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
+          </svg>
         </span>
         <ul
-          class="absolute top-8 right-0 p-4 bg-tertiary border border-secondary border-rounded rounded text-text"
+          class="absolute top-14 right-0 p-4 bg-white border border-secondary border-rounded rounded"
           :class="{'hidden': !profileMenuOpen}">
-          <li class="text-white hover:text-primary"><NuxtLink to="/profile">Profile</NuxtLink></li>
-          <li class="text-white hover:text-primary"><NuxtLink to="/logout">Logout</NuxtLink></li>
+          <li class="text-text hover:text-primary">
+            <NuxtLink to="/profile">Profile</NuxtLink>
+          </li>
+          <li class="text-text hover:text-primary">
+            <NuxtLink to="/logout">Logout</NuxtLink>
+          </li>
         </ul>
       </span>
     </nav>
