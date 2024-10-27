146f05d59b8b9534d3e2471843ce2462be9f4627
denis.music <deniss9oo@gmail.com>
2024-10-27 17:59:23 +0100

    Adjusted localStorage key

--- src/plugins/authentication.js
+++ src/plugins/authentication.js
@@ -1,6 +1,6 @@
 export default ({ store }) => {
   if (process.client) {
-    const user = localStorage.getItem('user');
+    const user = localStorage.getItem('user-logged');
     if (user) {
       store.dispatch('user/setLoggedUser', JSON.parse(user));
     }
