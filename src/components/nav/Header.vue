3d06a28e44fc16c5f035c57369dc8784d83ce5a8
denis.music <deniss9oo@gmail.com>
2024-11-23 15:26:46 +0100

    Fixed 1/2 width not rendering properly, changed from md to gl screens

--- src/components/nav/Header.vue
+++ src/components/nav/Header.vue
@@ -55,39 +55,39 @@ export default defineComponent({
       <div
         class="ipsc-nav-wrapper fixed top-0 right-[-100vw] w-full h-screen py-2 px-4 bg-white transition-all duration-300 ease-in-out text-right
         xs:right-[-75vw] xs:w-3/4
-        sm:right-[-50vw] sm:w-1-/2
-        md:static md:flex md:justify-end md:items-center md:gap-x-4 md:w-auto md:h-auto md:bg-transparent"
-        :class="{'translate-x-[-100vw] transform-gpu z-9 xs:translate-x-[-75vw] sm:translate-x-[-50vw]': isOpen}">
+        md:right-[-50vw] md:w-1/2
+        lg:static lg:flex lg:justify-end lg:items-center lg:gap-x-4 lg:w-auto lg:h-auto lg:bg-transparent"
+        :class="{'translate-x-[-100vw] transform-gpu z-9 xs:translate-x-[-75vw] md:translate-x-[-50vw]': isOpen}">
         <UButton
           icon="i-iconamoon-close-bold"
           color="tertiary"
           variant="link"
-          class="mb-2 md:hidden"
+          class="mb-2 lg:hidden"
           @click="toggleMenu"/>
         <nav class="ipsc-main-navigation flex flex-col justify-end py-2 border-t border-t-tertiary-300 border-b border-b-tertiary-300
-          md:flex-row md:items-center md:gap-x-4 md:border-t-0 md:border-b-0">
+          lg:flex-row lg:items-center lg:gap-x-4 lg:border-t-0 lg:border-b-0">
           <NuxtLink
             to="/"
-            class="nav-item mb-2 text-content hover:text-primary md:inline md:mb-0 md:text-white">
+            class="nav-item mb-2 text-content hover:text-primary lg:inline lg:mb-0 lg:text-white">
             {{ $t('main_menu.home') }}
           </NuxtLink>
           <NuxtLink
             v-if="!username"
             to="/login"
-            class="nav-item mb-2 text-content hover:text-primary md:inline md:mb-0 md:text-white">
+            class="nav-item mb-2 text-content hover:text-primary lg:inline lg:mb-0 lg:text-white">
             {{ $t('main_menu.login') }}
           </NuxtLink>
           <NuxtLink
             v-if="!username"
             to="/signin"
-            class="nav-item mb-2 text-content hover:text-primary md:inline md:mb-0 md:text-white">
+            class="nav-item mb-2 text-content hover:text-primary lg:inline lg:mb-0 lg:text-white">
             {{ $t('main_menu.signin') }}
           </NuxtLink>
           <span
             v-if="username"
-            class="block md:relative">
+            class="block lg:relative">
             <span
-              class="nav-item flex justify-end items-center text-content hover:text-primary md:justify-start cursor-pointer md:text-white"
+              class="nav-item flex justify-end items-center text-content hover:text-primary lg:justify-start cursor-pointer lg:text-white"
               @click="profileMenuToggle">
               <span class="mr-1">
                 {{ $t('main_menu.user_profile', username) }}
@@ -98,7 +98,7 @@ export default defineComponent({
                 variant="link"/>
             </span>
             <ul
-              class="block md:absolute md:top-14 md:right-0 md:p-4 md:bg-white md:border md:border-secondary md:border-rounded rounded"
+              class="block lg:absolute lg:top-14 lg:right-0 lg:p-4 lg:bg-white lg:border lg:border-secondary lg:border-rounded rounded"
               :class="{'hidden': !profileMenuOpen}">
               <li class="text-content hover:text-primary">
                 <NuxtLink to="/profile">
@@ -120,7 +120,7 @@ export default defineComponent({
         icon="i-iconamoon-menu-burger-horizontal-bold"
         color="primary"
         variant="outline"
-        class="md:hidden"
+        class="lg:hidden"
         @click="toggleMenu"/>
     </div>
   </header>
