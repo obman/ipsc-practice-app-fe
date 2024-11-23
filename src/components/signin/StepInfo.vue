96c6825cd9527bb55c59463c1239a64b2e828022
denis.music <deniss9oo@gmail.com>
2024-11-23 09:15:35 +0100

    Created StepInfo component for signin form, where one can pass different info of step form needed

---
+++ src/components/signin/StepInfo.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+name: "StepInfo"
+})
+</script>
+
+<template>
+  <div class="ipsc-signin-step-info">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
