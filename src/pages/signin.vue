ba76f7c2e64980cd4a99c8b36842435b9fac7826
denis.music <deniss9oo@gmail.com>
2024-11-24 16:23:39 +0100

    Separation of concerns : moved code, moved styles, moved transitions, created Form component

--- src/pages/signin.vue
+++ src/pages/signin.vue
@@ -1,79 +1,19 @@
 <script lang="ts">
-import { mapActions, mapState } from 'pinia';
+import { mapState } from 'pinia';
 
 export default defineComponent({
   data() {
     return {
-      errors: {
-        username: '',
-        email: '',
-        password: '',
-        rePassword: '',
-      },
       slideStepClass: 'next'
     }
   },
   computed: {
-    activeForm(): any {
-      switch(this.activeStep) {
-        case 1:
-          return resolveComponent('LazySigninFormStep1');
-        case 2:
-          return resolveComponent('LazySigninFormStep2');
-        case 3:
-          return resolveComponent('LazySigninFormStep3');
-      }
-    },
-    stepForm(): object {
-      switch(this.activeStep) {
-        case 1:
-          return this.step1;
-        case 2:
-          return this.step2;
-        case 3:
-          return this.step3;
-      }
-    },
-    stepErrors(): object {
-      switch(this.activeStep) {
-        case 1:
-          return this.errorsStep1;
-        case 2:
-          return this.errorsStep2;
-        case 3:
-          return this.errorsStep3;
-      }
-    },
-    ...mapState(useSigninWizard, ['step1', 'step2', 'step3', 'activeStep', 'errorsStep1', 'errorsStep2', 'errorsStep3']),
+    ...mapState(useSigninWizard, ['activeStep'])
   },
   methods: {
     setSlideStepAnimationClass(step: number): void {
       this.slideStepClass = step > this.activeStep ? 'next' : 'back';
     },
-    onActiveStep(step: number): void {
-      this.setSlideStepAnimationClass(step);
-      this.setActiveStep(step);
-    },
-    onFormChange(form, step: number): void {
-      this.setActiveStepForm(this.activeStep, form);
-      this.onActiveStep(step);
-    },
-    async onSubmit(event) {
-      event.preventDefault();
-      const payload = {
-        email: this.step1.email,
-        username: this.step1.username,
-        first_name: this.step2.firstName,
-        last_name: this.step2.lastName,
-        password: this.step3.password
-      };
-      this.fetchRegister(payload);
-      console.log (this.step1);
-      console.log (this.step2);
-      console.log (this.step3);
-    },
-    ...mapActions(useSigninWizard, ['setActiveStep', 'setActiveStepForm']),
-    ...mapActions(useUser, ['fetchRegister'])
   }
 })
 </script>
@@ -105,65 +45,6 @@ export default defineComponent({
         </div>
       </div>
     </div>
-    <form class="p-6 border border-secondary border-radius rounded">
-      <transition :name="`slide-${slideStepClass}`">
-        <component
-          :is="activeForm"
-          :step-form="stepForm"
-          :errors-bag="stepErrors"
-          @on-back="(form) => onFormChange(form, activeStep - 1)"
-          @on-next="(form) => onFormChange(form, activeStep + 1)"></component>
-      </transition>
-
-      <div v-if="activeStep === 4">
-        <UButton
-          icon="i-heroicons-chevron-left-16-solid"
-          size="md"
-          color="secondary"
-          variant="solid"
-          label="Back"
-          class="w-full mb-2 justify-center cursor-pointer"
-          @click="() => {onActiveStep(activeStep - 1); onFormChange(this.stepForm, activeStep) }"/>
-        <UButton
-          size="md"
-          color="secondary"
-          variant="solid"
-          type="submit"
-          label="Create account"
-          class="block w-full cursor-pointer"
-          @click="onSubmit($event)"/>
-      </div>
-    </form>
+    <SigninForm />
   </div>
 </template>
-
-<style scoped>
-.slide-next-enter-active,
-.slide-back-enter-active,
-.slide-next-leave-active,
-.slide-back-leave-active {
-  transition: all .6s ease;
-}
-
-.slide-next-enter-from {
-  height: 0;
-  opacity: 0;
-  transform: translateX(0);
-}
-.slide-next-leave-to {
-  height: 0;
-  opacity: 0;
-  transform: translateX(-1.5rem);
-}
-
-.slide-back-enter-from {
-  height: 0;
-  opacity: 0;
-  transform: translateX(0);
-}
-.slide-back-leave-to {
-  height: 0;
-  opacity: 0;
-  transform: translateX(1.5rem);
-}
-</style>
