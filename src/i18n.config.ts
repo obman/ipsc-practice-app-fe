d6bb582d60ebe4d16c445b02dbb017bbb381700e
denis.music <deniss9oo@gmail.com>
2024-11-24 10:33:59 +0100

    Added default local value

--- src/i18n.config.ts
+++ src/i18n.config.ts
@@ -3,6 +3,7 @@ import en from '@/i18n/en.json'
 // You can use `defineI18nConfig` to get type inferences for options to pass to vue-i18n.
 export default defineI18nConfig(() => {
   return {
+    defaultLocale: 'en',
     legacy: false,
     locale: 'en',
     lazy: false,
